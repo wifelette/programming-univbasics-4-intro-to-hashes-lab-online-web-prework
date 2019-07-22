4d2d16012abea3f713b15e4bef153390d8649b35
Leah Silber <leahsilber@gmail.com>
2019-07-22 05:06:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-	newHash = {
-		"key": "value"
-	}
+	newHash = {}
+	newHash[key] = value
   # return a hash that includes the key and value parameters passed into this method
 end
 
